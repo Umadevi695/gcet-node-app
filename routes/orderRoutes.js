a57e3d088e3b91ce3a847cfd352f01d2273f3525
Umadevi695 <uma970179@gmail.com>
2025-06-05 11:51:29 +0530

    updated

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -9,4 +9,14 @@ orderRouter.post("/new", async (req, res) => {
   return res.json(result);
 });
 
+orderRouter.get("/:id", async (req, res) => {
+  const email = req.params.id;
+  const result = await orderModel.find({ email });
+  return res.json(result);
+});
+
+orderRouter.get("/all", async (req, res) => {
+  const result = await orderModel.find();
+  return res.json(result);
+});
 export default orderRouter;
