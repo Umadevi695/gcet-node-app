bf529f1a425e6b45ddc5b008e46de6fbb08fe928
Umadevi695 <uma970179@gmail.com>
2025-06-07 19:37:25 +0530

    updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import productModel from "../models/productModel.js";
 
 const productRouter = express.Router();
@@ -7,6 +7,24 @@ productRouter.get("/all", async (req, res) => {
   const products = await productModel.find();
   res.json(products);
 });
+productRouter.post("/new", async (req, res) => {
+  const product = req.body;
+  const products = await productModel.create(product);
+  res.json(products);
+});
+
+export default productRouter;*/
+
+import express from "express";
+import productModel from "../models/productModel.js";
+import auth from "../middleware/auth.js";
+const productRouter = express.Router();
+
+productRouter.get("/all", auth, async (req, res) => {
+  const products = await productModel.find();
+  res.json(products);
+});
+
 productRouter.post("/new", async (req, res) => {
   const product = req.body;
   const products = await productModel.create(product);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
@@ -39,6 +39,40 @@ userRouter.get("/:id/name", async (req, res) => {
   const email = req.params.id;
   const result = await userModel.findOne({ email }, { _id: 0, name: 1 });
   return res.json(result);
+});
+
+export default userRouter;*/
+
+import express from "express";
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+const userRouter = express.Router();
+import jwt from "jsonwebtoken";
+const SECRET_KEY = "helloworld";
+
+userRouter.post("/register", async (req, res) => {
+  const { name, email, pass } = req.body;
+  // const hashpassword = await bcrypt.hash(pass, 10);
+  const result = await userModel.create({
+    name: name,
+    email: email,
+    pass: pass,
+    //  pass: hashpassword,
+  });
+  return res.json(result);
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
+  const result = await userModel.findOne({ email });
+  if (!result) return res.json({ message: "Invalid user" });
+  // const matchPassword = await bcrypt.compare(pass, result.pass);
+  // if (!matchPassword) {
+  //   return res.status(400).json({ message: "Invalid Password" });
+  // }
+  const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
+  console.log(result);
+  return res.json({ user: result, token: token });
 });
 
 export default userRouter;
